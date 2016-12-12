b2542bb0f91ee7f1c9dfc5f29d609d57a9b49f73
David Dorr <shishkebab101@hotmail.com>
2016-12-12 18:57:36 +0200

    first dabs at calculator,.. with some first thoughts. needs development.

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,33 +1,75 @@
 
 var operator,
+	tempnums = "",
 	nums = [],
 	total = 0;
+	
+
+// needs zero button
+// needs to sum up to allow chaining
+// CSS needs dealing with
+// needs work. but is good start
+// to many global variables being operated on by functions.
+// this all needs a rethink.????
+// perhaps even have a calculator constructor and do it OOP style??
+
 
 $(function() {
+	display(total);
+
 	$('.num-btn').click(function() {
-		nums.push(parseInt($(this).val()));
+
+		tempnums += $(this).val();
+		display(tempnums);
 	});
 
-	$('.num-op').click(function() {
+	// needs work. chaining works. but value needs to be stored to carry on operations
+	// go through the methodlogy again.
+	$('.num-op').click(function() { 
+		nums.push(parseFloat(tempnums));
+		tempnums = "";
+		if (nums.length >=2) {
+			nums[0] = calculate(operator, nums);
+			nums.splice(1, 1);
+		}
 		operator = $(this).val();
 	});
 
 	$('#btn-calc').click(function() {
+		nums.push(parseFloat(tempnums));
+		tempnums = "";
 		total = calculate(operator, nums);
-		$('#output').html(total);
+		display(total);
+	});
+
+	$('#btn-clear').click(function() {
+		resetAll();
 	});
 });
 
+// needs default senario
 function calculate(operator, myArray) {
 	switch(operator) {
 		case 'plus': return myArray[0] + myArray[1]; break;
 		case 'minus': return myArray[0] - myArray[1]; break;
+		case 'times': return myArray[0] * myArray[1]; break;
+		case 'divide': return myArray[0] / myArray[1]; break;
 	}
 }
 
-// $(function() {
-// 	$('.btn').click(function() {
-// 		var num = $(this).val();
-// 		$('#output').html(num);
-// 	});
-// });
+function resetAll() { 
+	operator = "";
+	tempnums = ""
+	nums = [];
+	total = 0;
+	display(total);
+}
+
+function display(outstr) {
+	$('#output').html(outstr);
+}
+
+// need something like together with a "c" button
+function resetTempnums() {
+	tempnums = "";
+}
